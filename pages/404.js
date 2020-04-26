caeac1d4cda0b5b50313aca78cdc07aa23fbbb11
GabrielHorbach <gabrielhorbachmachado@hotmail.com>
2020-04-26 01:06:45 -0300

    first commit

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <h1 className={utilStyles.headingXl}>404 - This page doesn't exist</h1>
+    </Layout>
+  )
+}
