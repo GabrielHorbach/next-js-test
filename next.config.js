4342ab0a619d8e8f80486d4b1c40afb45cd7524c
GabrielHorbach <gabrielhorbachmachado@hotmail.com>
2020-05-15 17:32:22 -0300

    remove unused config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-const isProd = (process.env.NODE_ENV || 'production') === 'production'
-
 module.exports = {
-  exportPathMap: () => ({
-    '/': { page: '/' },
-  }),
-  assetPrefix: isProd ? '/your-repository-name' : '',
   devIndicators: {
     autoPrerender: false,
   },
